eeff71aa736b099f67ad5db74bcbec40b4f6150f
abedassoum <75977129+abedassoum@users.noreply.github.com>
2023-09-14 21:41:02 +0200

    server.js

--- src/server.js
+++ src/server.js
@@ -1,2 +1,14 @@
 import express from "express";
 import cors from "cors";
+import router from "./router.js";
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(cors());
+app.use(router);
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
