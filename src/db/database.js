981e8d40f285e3bb1bf99229845d3d3d0e103389
Jack <jackvwh@hotmail.com>
2023-09-19 10:04:23 +0200

    Digicert

--- src/db/database.js
+++ src/db/database.js
@@ -1,10 +1,18 @@
 import mysql from 'mysql2';
+import 'dotenv/config';
+import fs from 'fs';
 
-export const con = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: 'jackKcaj',
-  database: 'MusicBase',
+export const connection = mysql.createConnection({
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  user: procces.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
   multipleStatements: true,
 });
+
+if (procces.env.MYSQL_CERT) {
+  connection.config.ssl = {
+    ca: fs.readFileSync(procces.env.MYSQL_CERT),
+  };
+}
