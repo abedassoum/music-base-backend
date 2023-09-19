addbea3f3b9e8ae933c0d1ad7d1cc926863857f5
Jack <jackvwh@hotmail.com>
2023-09-19 12:01:57 +0200

    db fix

--- src/db/database.js
+++ src/db/database.js
@@ -2,18 +2,21 @@ import mysql from 'mysql2';
 import 'dotenv/config';
 import fs from 'fs';
 
-const connection = mysql.createConnection({
+const dbconfig = {
   host: process.env.MYSQL_HOST,
   port: process.env.MYSQL_PORT,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
   multipleStatements: true,
-});
+};
 
 if (process.env.MYSQL_CERT) {
-  connection.ssl = {
+  dbconfig.ssl = {
     cs: fs.readFileSync('DigiCertGlobalRootG2.crt.pem'),
   };
 }
+
+const connection = mysql.createConnection(dbconfig);
+
 export default connection;
