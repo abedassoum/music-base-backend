93c2d65428969ea6fd7058172e85d7b7582c3a7f
Jack <jackvwh@hotmail.com>
2023-09-19 11:51:08 +0200

    database fix

--- src/db/database.js
+++ src/db/database.js
@@ -1,20 +1,19 @@
 import mysql from 'mysql2';
-import 'dotenv/config'
+import 'dotenv/config';
 import fs from 'fs';
 
 const connection = mysql.createConnection({
-  host: process.env.MYSQL_HOST, 
-  port: process.env.MYSQL_PORT, 
-  user: process.env.MYSQL_USER, 
-  password: process.env.MYSQL_PASSWORD, 
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
   multipleStatements: true,
 });
 
-if (process.env.MYSQL_CERT){
-  connection.config.ssl = {
-    ca: fs.readFileSync(procces.env.MYSQL_CERT)
-  }
+if (process.env.MYSQL_CERT) {
+  connection.ssl = {
+    cs: fs.readFileSync('DigiCertGlobalRootG2.crt.pem'),
+  };
 }
-
 export default connection;
