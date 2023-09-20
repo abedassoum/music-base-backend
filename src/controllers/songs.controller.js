c97404e50252772cd43776f561a1df2d2333223c
abedassoum <75977129+abedassoum@users.noreply.github.com>
2023-09-20 17:48:01 +0200

    controllers for songs needs edit and testing

--- src/controllers/songs.controller.js
+++ src/controllers/songs.controller.js
@@ -5,40 +5,94 @@ import {
   createSong_db,
   deleteSong_db,
 } from '../models/songs.models.js';
+  
 
-export function readAllSongs(req, res) {
-  const songs = readAllSongs_db();
-  if (err) throw err;
-  res.status(200).json(songs);
+export async function readAllSongs(req, res) {
+  const songs = await readAllSongs_db();
+  if (!songs) {
+    res.status(500).json({ error: 'Failed to retrieve songs' });
+  } else {
+    res.status(200).json(songs);
+  }
 }
 
-export function readSongById(req, res) {
+export async function readSongById(req, res) {
   const id = req.params.id;
-  const song = readSongById_db(id);
-  if (err) throw err;
-  res.status(200).json(song);
+  const song = await readSongById_db(id);
+  if (!song) {
+    res.status(404).json({ error: 'Song not found' });
+  } else {
+    res.status(200).json(song);
+  }
 }
 
-export function updateSong(req, res) {
+export async function updateSong(req, res) {
   const id = req.params.id;
   const { name, year, image, artist, genres, labels } = req.body;
 
-  const song = updateSong_db(id, name, year, image, artist, genres, labels);
-  if (err) throw err;
-  res.status(200).json(song);
+  const updatedSong = await updateSong_db(id, name, year, image, artist, genres, labels);
+  if (!updatedSong) {
+    res.status(500).json({ error: 'Failed to update song' });
+  } else {
+    res.status(200).json(updatedSong);
+  }
 }
 
-export function createSong(req, res) {
+export async function createSong(req, res) {
   const { name, year, image, artist, genres, labels } = req.body;
 
-  const song = createSong_db(name, year, image, artist, genres, labels);
-  if (err) throw err;
-  res.status(200).json(song);
+  const newSong = await createSong_db(name, year, image, artist, genres, labels);
+  if (!newSong) {
+    res.status(500).json({ error: 'Failed to create song' });
+  } else {
+    res.status(200).json(newSong);
+  }
 }
 
-export function deleteSong(req, res) {
+export async function deleteSong(req, res) {
   const id = req.params.id;
-  const song = deleteSong_db(id);
-  if (err) throw err;
-  res.status(200).json(song);
+  const deletedSong = await deleteSong_db(id);
+  if (!deletedSong) {
+    res.status(500).json({ error: 'Failed to delete song' });
+  } else {
+    res.status(200).json(deletedSong);
+  }
 }
+
+
+// export function readAllSongs(req, res) {
+//   const songs = readAllSongs_db();
+//   if (err) throw err;
+//   res.status(200).json(songs);
+// }
+
+// export function readSongById(req, res) {
+//   const id = req.params.id;
+//   const song = readSongById_db(id);
+//   if (err) throw err;
+//   res.status(200).json(song);
+// }
+
+// export function updateSong(req, res) {
+//   const id = req.params.id;
+//   const { name, year, image, artist, genres, labels } = req.body;
+
+//   const song = updateSong_db(id, name, year, image, artist, genres, labels);
+//   if (err) throw err;
+//   res.status(200).json(song);
+// }
+
+// export function createSong(req, res) {
+//   const { name, year, image, artist, genres, labels } = req.body;
+
+//   const song = createSong_db(name, year, image, artist, genres, labels);
+//   if (err) throw err;
+//   res.status(200).json(song);
+// }
+
+// export function deleteSong(req, res) {
+//   const id = req.params.id;
+//   const song = deleteSong_db(id);
+//   if (err) throw err;
+//   res.status(200).json(song);
+// }
