0dd035fe6829868265173b7a94401e1069591c3f
Dennis2300 <124138161+Dennis2300@users.noreply.github.com>
2023-09-22 18:45:30 +0200

    readdAllAlbum and readAlbumById is done

--- src/models/albums.models.js
+++ src/models/albums.models.js
@@ -1,31 +1,89 @@
 import connection from '../db/database.js';
 
-export async function readAllAlbums_db() {
+async function query(sql, params) {
   return new Promise((resolve, reject) => {
-    connection.query('SELECT * FROM albums', (err, results) => {
+    connection.query(sql, params, (err, results) => {
       if (err) {
-        reject(err);
+        reject(err)
       } else {
         resolve(results);
       }
-    });
-  });
+    })
+  })
 }
 
-export function readAlbumById_db(id) {
-  return new Promise((resolve, reject) => {
-    connection.query(
-      'SELECT * FROM songs WHERE id = ?',
-      [id],
-      (err, results) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(results);
-        }
-      }
-    );
-  });
+export async function readAllAlbums_db() {
+  const sql = `
+  SELECT albums.*,
+    GROUP_CONCAT(DISTINCT artists.name) AS artists,
+      GROUP_CONCAT(DISTINCT labels.name) AS labels,
+        GROUP_CONCAT(DISTINCT genres.name) AS genres,
+          GROUP_CONCAT(DISTINCT songs.title) AS songs
+  FROM albums
+  LEFT JOIN album_artist ON albums.id = album_artist.album_id
+  LEFT JOIN artists ON album_artist.artist_id = artists.id
+
+  LEFT JOIN album_label ON albums.id = album_label.album_id
+  LEFT JOIN labels ON album_label.label_id = labels.id
+
+  LEFT JOIN album_genre ON albums.id = album_genre.album_id
+  LEFT JOIN genres ON album_genre.genre_id = genres.id
+
+  LEFT JOIN song_album ON albums.id = song_album.album_id
+  LEFT JOIN songs ON song_album.song_id = songs.id
+  GROUP BY albums.id
+  `;
+
+  try {
+    const results = await query(sql);
+    return results;
+  } catch (error) {
+    console.error('Error  reading all albums', error);
+    throw error;
+  }
+}
+
+export async function readAlbumById_db(id) {
+  const sql = `
+  SELECT albums.*,
+    GROUP_CONCAT(DISTINCT artists.name) AS artists,
+      GROUP_CONCAT(DISTINCT labels.name) AS labels,
+        GROUP_CONCAT(DISTINCT genres.name) AS genres,
+          GROUP_CONCAT(DISTINCT songs.title) AS songs
+  FROM albums
+  LEFT JOIN album_artist ON albums.id = album_artist.album_id
+  LEFT JOIN artists ON album_artist.artist_id = artists.id
+
+  LEFT JOIN album_label ON albums.id = album_label.album_id
+  LEFT JOIN labels ON album_label.label_id = labels.id
+
+  LEFT JOIN album_genre ON albums.id = album_genre.album_id
+  LEFT JOIN genres ON album_genre.genre_id = genres.id
+
+  LEFT JOIN song_album ON albums.id = song_album.album_id
+  LEFT JOIN songs ON song_album.song_id = songs.id
+  WHERE albums.id = ?
+  GROUP BY albums.id
+  `;
+
+  try {
+    const results = await query(sql, [id]);
+
+    if (results.length === 0) {
+      return null;
+    }
+
+    const album = results[0]
+
+    album.artists = album.artists ? album.artists.split(',') : [];
+    album.labels = album.labels ? album.labels.split(',') : [];
+    album.genres = album.genres ? album.genres.split(',') : [];
+    album.songs = album.songs ? album.songs.split(',') : [];
+    return album;
+  } catch (error) {
+    console.error('Error getting album', error);
+    throw error;
+  }
 }
 
 export function updateAlbum_db(title, releaseDate, genre, id) {
